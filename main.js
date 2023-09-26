3df245d0c8a768ffc9d5638ab0e52ef622052fad
bee4rt <info.visuel@gmail.com>
2023-09-26 22:02:47 +0300

    add functions

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 const result = document.querySelector("#result");
 const btns = document.querySelectorAll("button");
+
+// functions
+
+//clear function
+function clearResult() {}
+
+//calculate function
+function calculateResult() {}
+
+//append function
+function appendValue(btnValue) {}
+
+///////////////////////////////////////////////////////
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let btnValue = btns[i].textContent;
+    if (btnValue === "AC") {
+      clearResult();
+    } else if (btnValue === "=") {
+      calculateResult();
+    } else {
+      appendValue();
+    }
+  });
+}
